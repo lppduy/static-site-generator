99940955e6a721a0997468781ac7becf691dc1af
lppduy <lephamphuongduy@gmail.com>
2024-06-04 13:58:17 +0700

    feat: text to text node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,58 +2,52 @@ class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.children = children if children is not None else []
-        self.props = props if props is not None else {}
+        self.children = children
+        self.props = props
 
     def to_html(self):
-        raise NotImplementedError("Child classes should implement this method to render themselves as HTML.")
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
-        return " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
-        return (f"HTMLNode(tag={self.tag!r}, value={self.value!r}, "
-                f"children={self.children!r}, props={self.props!r})")
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
-class ParentNode(HTMLNode):
-    def __init__(self, tag=None, children=None, props=None):
-        if tag is None:
-            raise ValueError("ParentNode requires a tag.")
-        if children is None or not children:
-            raise ValueError("ParentNode requires at least one child.")
-        super().__init__(tag=tag, children=children, props=props)
-
-    def to_html(self):
-        if not self.children:
-            raise ValueError("ParentNode requires at least one child.")
-
-        props_html = self.props_to_html()
-        children_html = "".join(child.to_html() for child in self.children)
-        return f"<{self.tag}{props_html}>{children_html}</{self.tag}>"
-
-    def __repr__(self):
-        return (f"ParentNode(tag={self.tag!r}, children={self.children!r}, "
-                f"props={self.props!r})")
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
-        if value is None:
-            raise ValueError("LeafNode requires a value.")
-        super().__init__(tag=tag, value=value, children=None, props=props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value is None:
-            raise ValueError("LeafNode requires a value.")
-        
+            raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
-        
-        props_html = self.props_to_html()
-        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
     def __repr__(self):
-        return (f"LeafNode(tag={self.tag!r}, value={self.value!r}, "
-                f"props={self.props!r})")
-    
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,37 +1,8 @@
 from textnode import TextNode
-from htmlnode import HTMLNode, LeafNode, ParentNode
-from text_to_html import text_node_to_html_node
-from split_nodes_delimiter import split_nodes_delimiter
 
 def main():
-  # text_node = TextNode("Hello", "bold", "https://www.google.com")
-  # print(text_node)
-  # html_node = HTMLNode("p", "This is a paragraph.", {"class": "paragraph"})
-  # print(html_node)
-  # leaf_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-  # print(leaf_node)
-  # print(leaf_node.to_html())
-  # parent_node = ParentNode(
-  #   "p",
-  #   [
-  #       LeafNode("b", "Bold text"),
-  #       LeafNode(None, "Normal text"),
-  #       LeafNode("i", "italic text"),
-  #       LeafNode(None, "Normal text"),
-  #   ],
-  # )
-  # print(parent_node.to_html())
-
-  # text_node = TextNode(text="Click here", text_type="link", url="https://www.example.com")
-  # html_node = text_node_to_html_node(text_node)
-  # print(html_node.to_html())
-
-  node = TextNode("This is text with a `code block` word", 'text')
-  new_nodes = split_nodes_delimiter([node], "`", 'code')
-
-
-  for node in new_nodes:
-      print(node)
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
 
 
 main()
